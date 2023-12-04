81d05c52982833938fba998aef42684efc34faf5
shri234 <43914954+shri234@users.noreply.github.com>
2023-12-05 03:09:22 +0530

    Update login.ts

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const loginPost = async (body: any) =>
   await axios
-    .post(`${process.env.REACT_APP_IP}/user/login`, body)
+    // .post(`${process.env.REACT_APP_IP}/user/login`, body)
+  .post("https://moneyminninggame.cloud/user/login", body)
      .then((res) => res)
     .catch((error) => error);
