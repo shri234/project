18d617a5631a7e52f60527d2669380dcae6f1165
shri234 <43914954+shri234@users.noreply.github.com>
2023-12-09 13:07:21 +0530

    Update SignUp.tsx

--- src/components/sign-up/SignUp.tsx
+++ src/components/sign-up/SignUp.tsx
@@ -214,7 +214,7 @@ const SignUpComponent: React.FC = () => {
             type="text"
             value={refer_id}
             onChange={(e) => setReferralId(e.target.value)}
-            className={formErrors.refer_id ? "invalid" : ""}
+            className={formErrors.refer_id ? "invalid" : null}
             placeholder="Enter your referral ID"
           />
           {formErrors.refer_id && (
