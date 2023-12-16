b9a201a167214cecbcde7102ef318bd8a7c69947
shri234 <43914954+shri234@users.noreply.github.com>
2023-12-16 18:45:01 +0530

    Update winningTicket.ts

--- src/swr/winningTicket.ts
+++ src/swr/winningTicket.ts
@@ -2,14 +2,14 @@ import useSWR from "swr";
 import { fetcher } from "./wallet_ticket_count";
   const now =new Date();
 
-  if(now.getHours()==18 && now.getMinutes()==45){
-function useWinningTicket(path: string) {
 
+function useWinningTicket(path: string) {
+  if(now.getHours()==18 && now.getMinutes()==45){
   const { data, mutate, isValidating, error } = useSWR<any>(
     `${process.env.REACT_APP_IP}/ticket/get-${path}-result`,
     fetcher
   );
-  
+  }
 
   return {
     user_winning_ticket: data,
@@ -18,7 +18,7 @@ function useWinningTicket(path: string) {
     isValidating,
     userWinningTicketRefetch: mutate,
   };
-  }
+  
 }
 
 export default useWinningTicket;
